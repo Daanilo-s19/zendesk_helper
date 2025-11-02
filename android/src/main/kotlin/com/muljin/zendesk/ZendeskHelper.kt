9c53dc352ee2f7fdb0ad81b9579d4e037bec9506
José Cleiton Correia <jcleitonbc@gmail.com>
2025-11-01 22:39:21 -0300

    fix: update Zendesk dependencies to latest versions and clean up code style

--- android/src/main/kotlin/com/muljin/zendesk/ZendeskHelper.kt
+++ android/src/main/kotlin/com/muljin/zendesk/ZendeskHelper.kt
@@ -1,7 +1,6 @@
 package com.muljin.zendesk
 
 import android.app.Activity
-import androidx.annotation.NonNull
 import com.zendesk.logger.Logger
 import com.zendesk.service.ErrorResponse
 import com.zendesk.service.ZendeskCallback
@@ -14,7 +13,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import zendesk.chat.*
 import zendesk.classic.messaging.MessagingActivity
-import java.lang.reflect.Method
+import zendesk.core.BuildConfig
 
 
 /** ZendeskHelper */
@@ -26,12 +25,12 @@ class ZendeskHelper : FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel: MethodChannel
   private lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "zendesk")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 
@@ -48,7 +47,7 @@ class ZendeskHelper : FlutterPlugin, MethodCallHandler, ActivityAware {
   override fun onDetachedFromActivityForConfigChanges() {
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
 
 try {
 
@@ -104,7 +103,7 @@ try {
 
     }
 }catch (e:Exception){
-  result.error("unKnowen",e.message,e.toString());
+  result.error("unKnowen",e.message,e.toString())
 }
   }
 
@@ -183,7 +182,7 @@ try {
           .withEngines(ChatEngine.engine())
           .show(activity, chatConfiguration)
 }catch ( e:Exception){
- throw e;
+ throw e
 }
 
   }
@@ -248,7 +247,7 @@ try {
   private fun setIdentity(call: MethodCall) {
     val jwtToken = call.argument<String>("token")
     if (jwtToken.isNullOrEmpty()){
-      return;
+      return
     }
 
     Chat.INSTANCE.setIdentity { jwtCompletion -> jwtCompletion?.onTokenLoaded(jwtToken); }
